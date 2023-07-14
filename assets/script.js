6ee86015d894d57ac90c1d5545fb8a81c73a1d1f
justinschoi93 <justinschoi93@gmail.com>
2023-07-13 22:52:56 -0700

    deleted keydown listener. Created search button. Defined cb function. Still debugging cb function. getCoordinates(cityEl) won't save to variable.

--- assets/script.js
+++ assets/script.js
@@ -4,39 +4,36 @@
 var searchHistory = $('.search-history');
 //when the return key is hit, the text content within the search bar will be added to the search history
 //
-var searchBarEl = $('.search-bar').on('keydown', function (event){
-    if (event.key === 'Enter'){
-        // console.log(event);
-        // console.log(searchBarEl.val());
-        var cityEl = $(searchBarEl).val();
-        var newCityEl = document.createElement('li');
-        newCityEl.textContent = cityEl;
-        searchHistory.append(newCityEl);
+var searchBarEl = $('.search-bar');
+var submitBtn = $('.submit');
 
-        var coordinates = getCoordinates(cityEl);
-        console.log(getCoordinates(cityEl));
-        // console.log(coordinates);
-        // checkWeather(coordinates[0], coordinates[1]);
-    }
+submitBtn.on('click', function (){
+    var cityEl = searchBarEl.val();
+    var newCityEl = document.createElement('li');
+    newCityEl.textContent = cityEl;
+    // console.log(newCityEl)
     
-});
+    searchHistory.append(newCityEl);
+    
+    //why aren't the coordinates getting saved to the result variable?
+    var result = getCoordinates(cityEl);
+    console.log(result);
+    
+    
+})
 
 
-function getCoordinates (cityName){
-    var requestURL = 'https://api.openweathermap.org/geo/1.0/direct?q=' + cityName + '&limit=&appid=0b2949d3ba17a6aec298126cb969f7dc';
-    var result = [];
+function getCoordinates (cityEl){
+    var requestURL = 'https://api.openweathermap.org/geo/1.0/direct?q=' + cityEl + '&limit=&appid=0b2949d3ba17a6aec298126cb969f7dc';
+    
     
     fetch(requestURL)
     .then(function(response){
         return response.json();
     })
     .then(function(data){
-        
-        result = [data[0].lat, data[0].lon];
-        // console.log(result);
+        var result = [data[0].lat, data[0].lon];
         return result;
-       
-
     })
 };
 
