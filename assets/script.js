295bc370795d24a662748b68114ed460171c1dcd
Justin 석원 Choi <justinschoi93@gmail.com>
2024-03-16 23:48:32 -0700

    fully functional. moving onto styling

--- assets/script.js
+++ assets/script.js
@@ -15,44 +15,58 @@ const displayWindDirection = document.getElementById('wind-direction-display');
 const displayHumidity = document.getElementById('humidity-display');
 const displayPressure = document.getElementById('pressure-display');
 const recentHistory = document.getElementById('recent-history');
-const measurementUnit = document.getElementById('measurement-units').value;
 const countrySelect = document.getElementById('country-select');
 const stateSelect = document.getElementById('state-select');
-
+const unitSelect = document.getElementById('unit-select');
+const fiveDayForecast = document.getElementById('five-day-forecast');
 
 COUNTRIES.forEach( country => {
     const optionCountry = new Option(country.name, country['country-code']);
     countrySelect.appendChild(optionCountry); 
 });
-document.addEventListener('DOMContentLoaded', async (event) => {
-    event.preventDefault();
-    // Search Bar including submit button
-    const searchBar = document.getElementById("search-button")
-                            .addEventListener( 'click', (event) => {
-                                event.preventDefault();
-                                const searchBarInput = document.getElementById('city-search').value;
-                                const countryCode = countrySelect.options[countrySelect.selectedIndex].value;
-                                const state = stateSelect.options[stateSelect.selectedIndex].value;
-                                getCoordinates(searchBarInput, state, countryCode);
-                            });
 
+function isValidZipCode(zipCode) {
+    return /(^\d{5}$)|(^\d{5}-\d{4}$)/.test(zipCode);
+  }
+// Function that defines the geocoding api based on the input
+function getGeocodingAPI (searchBarInput, state, countryCode, unit) {
+    let geocoding_api = '';
 
-    // Function that finds coordinates of city by either city name or zip code
-    function getCoordinates (searchBarInput, state, countryCode) {
-        let geocoding_api = ''
+    if (isValidZipCode(searchBarInput)) { // if input is a zip code
+        let zipcode = searchBarInput;
+        geocoding_api = `http://api.openweathermap.org/geo/1.0/zip?zip=${zipcode}&appid=0b2949d3ba17a6aec298126cb969f7dc`
+    } else if (state) {
+        let city = searchBarInput;
+        geocoding_api = `http://api.openweathermap.org/geo/1.0/direct?q=${city},${state},${countryCode}&appid=0b2949d3ba17a6aec298126cb969f7dc`
+    } else {
+        let city = searchBarInput;
+        geocoding_api = `http://api.openweathermap.org/geo/1.0/direct?q=${city},${countryCode}&appid=0b2949d3ba17a6aec298126cb969f7dc`
+    }
+    return geocoding_api;
+};
+// Function that finds coordinates of city by either city name or zip code
+function getCoordinates (geocodingAPI) {
 
-        if (typeof searchBarInput === 'number' && searchBarInput.toString().length === 5) {
-            let zipcode = searchBarInput;
-            geocoding_api = `http://api.openweathermap.org/geo/1.0/zip?zip=${zipcode},${countryCode}&appid=0b2949d3ba17a6aec298126cb969f7dc`
-        } else if (state) {
-            let city = searchBarInput;
-            geocoding_api = `http://api.openweathermap.org/geo/1.0/direct?q=${city},${state},${countryCode}&appid=0b2949d3ba17a6aec298126cb969f7dc`
-        } else {
-            let city = searchBarInput;
-            geocoding_api = `http://api.openweathermap.org/geo/1.0/direct?q=${city},${countryCode}&appid=0b2949d3ba17a6aec298126cb969f7dc`
-        }
+    console.log(geocodingAPI)
 
-        fetch(geocoding_api)
+    if (geocodingAPI.includes('zip')) {
+        fetch(geocodingAPI)
+            .then( response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok. Could not get coordinates.');
+                }
+                return response.json();
+            })
+            .then( data => {
+                console.log(data)
+                let lat = data.lat;
+                let lon = data.lon;
+                let name = data.name;
+                
+                checkWeather(lat, lon, name);
+            })
+    } else {
+        fetch(geocodingAPI)
             .then( response => {
                 if (!response.ok) {
                     throw new Error('Network response was not ok. Could not get coordinates.');
@@ -63,78 +77,135 @@ document.addEventListener('DOMContentLoaded', async (event) => {
                 if (data && data.length > 0) {
                     let lat = data[0].lat;
                     let lon = data[0].lon;
-                    
-                    checkWeather(lat, lon);
+                    let name = data[0].name;
+                    console.log(name)
+                    checkWeather(lat, lon, name);
                 }
             })
     }
+}
+// Function that checks the weather using coordinates
+function checkWeather (lat, lon, name) {
+    const unit = unitSelect.options[unitSelect.selectedIndex].value;
+    const weather_api = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&units=${unit}&appid=0b2949d3ba17a6aec298126cb969f7dc`;
+    
+    fetch(weather_api)
+        .then( response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok. Could not check weather.')
+            } else {
+                return response.json()
+            }
+        })
+        .then( data => {
+            
+            console.log(data);
+            // current weather
+            let currentTemp = data.current.temp;
+            let currentTempMax = data.daily[0].temp.max;
+            let currentTempMin = data.daily[0].temp.min;
+            let currentFeelsLike = data.current.feels_like;
+            let currentHumidity = data.current.humidity;
+            let currentWindDirection = data.current.wind_deg;
+            let currentWindSpeed = data.current.wind_speed;
+            let currentClouds = data.current.clouds;
+            let currentVisibility = data.current.visibility;
+            let currentPressure = data.current.pressure;
+            let currentSunrise = data.current.sunrise;
+            let currentSunset = data.current.sunset;
+            let currentDescription = data.current.weather[0].description;
+            let currentIcon = data.current.weather[0].icon;
 
-    function checkWeather (lat, lon) {
-        
-        const weather_api = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=0b2949d3ba17a6aec298126cb969f7dc`;
-        const fivedayforecast_api = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=0b2949d3ba17a6aec298126cb969f7dc`
-        
-        fetch(weather_api)
-            .then( response => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok. Could not check weather.')
-                } else {
-                    return response.json()
-                }
+            // 5 day forecast
+            data.daily.forEach( (day, index) => {
+
+                const forecast = {
+                    day: index,
+                    dayFeelsLike: day.feels_like.day,
+                    nightFeelsLIke: day.feels_like.night,
+                    eveFeelsLike: day.feels_like.eve,
+                    mornFeelsLike: day.feels_like.morn,
+                    humidity: day.humidity,
+                    moonPhase: day.moon_phase,
+                    moonrise: day.moonrise,
+                    moonset: day.moonset,
+                    summary: day.summary,
+                    sunrise: day.sunrise,
+                    sunset: day.sunset,
+                    dayTemp: day.temp.day,
+                    nightTemp: day.temp.night,
+                    eveTemp: day.temp.eve,  
+                    mornTemp: day.temp.morn,
+                    lowTemp: day.temp.min,
+                    highTemp: day.temp.max,
+                    windDirection: day.wind_deg,
+                    windSpeed: day.wind_speed,
+                    clouds: day.clouds,
+                };
+
+                if (index === 0) {forecast.day = 'Today'};
+                if (index === 1) {forecast.day = 'Tomorrow'};
+                if (index === 2) {forecast.day = 'Day after tomorrow'};
+                if (index === 3) {forecast.day = 'Day after the day after tomorrow'};
+                if (index === 4) {forecast.day = '4 days from now'};
+                if (index === 5) {forecast.day = '5 days from now'};
+                if (index === 6) {forecast.day = '6 days from now'};
+                if (index === 7) {forecast.day = '7 days from now'}
+                
+
+                let weatherCard = document.createElement("div");
+                weatherCard.setAttribute("id", `day-${index}`);
+                
+                Object.entries(forecast).forEach( ([key, value]) => {
+                    let entry = document.createElement("div");
+                    entry.setAttribute("id", `day-${index}-${key}`);
+                    entry.innerHTML = value;    
+                    weatherCard.appendChild(entry);
+                })
+
+                let weatherIcon = document.createElement("img");
+                weatherIcon.src = "http://openweathermap.org/img/wn/" + day.weather[0].icon + ".png";
+                weatherCard.appendChild(weatherIcon);
+                fiveDayForecast.appendChild(weatherCard);
             })
-            .then( data => {
-                
-                // convert to desired measurement unit
-                if (measurementUnit === 'farenheit') {
-                    data.main.temp = parseFloat((data.main.temp - 273.15) * 9/5 + 32).toFixed(1);
-                    data.main.feels_like = parseFloat((data.main.feels_like - 273.15) * 9/5 + 32).toFixed(1);
-                    data.main.temp_max = parseFloat((data.main.temp_max - 273.15) * 9/5 + 32).toFixed(1);
-                    data.main.temp_min = parseFloat((data.main.temp_min - 273.15) * 9/5 + 32).toFixed(1);
-                } else if (measurementUnit === 'celsius') {
-                    data.main.temp = parseFloat(data.main.temp - 273.15).toFixed(1);
-                    data.main.feels_like = parseFloat(data.main.feels_like - 273.15).toFixed(1);
-                    data.main.temp_max = parseFloat(data.main.temp_max - 273.15).toFixed(1);
-                    data.main.temp_min = parseFloat(data.main.temp_min - 273.15).toFixed(1);
-                }
 
-                // display data
-                displayName.innerHTML = data.name;
-                displayIcon.setAttribute('src', `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`);
-                displayTemperature.innerHTML = `Current Temperature: ${data.main.temp}`;
-                displayFeelsLike.innerHTML = `Feels like: ${data.main.feels_like}`;
-                displayTempMax.innerHTML = `High: ${data.main.temp_max}`;
-                displayTempMin.innerHTML = `Low: ${data.main.temp_min}`;
-                displayDescription.innerHTML = data.weather[0].description;
-                displaySunrise.innerHTML = `Sunrise: ${data.sys.sunrise}`;
-                displaySunset.innerHTML = `Sunset: ${data.sys.sunset}`;
-                displayWindSpeed.innerHTML = `Wind speed: ${data.wind.speed}`;
-                displayWindDirection.innerHTML = `Wind direction: ${data.wind.deg}`;
-                displayHumidity.innerHTML = `Humidity: ${data.main.humidity}`;
-                displayPressure.innerHTML = `Pressure: ${data.main.pressure}`;
-                
-                // Add new city to recent history
-                const searchedCitiesList = Array.from(recentHistory.querySelectorAll("li.searched-city"));
-                const searchedCitiesIds = searchedCitiesList.map( city => city.getAttribute("id"));
-                
-                if (!searchedCitiesIds.includes(data.name)) {
-                    let searchedCity = document.createElement("li");
-                    searchedCity.setAttribute("class", "searched-city");
-                    searchedCity.setAttribute("id", data.name);
-                    searchedCity.innerHTML = data.name;
-                    recentHistory.appendChild(searchedCity)
-                }
-                
-                // console.log(searchedCitiesIds);
-                // console.log(searchedCity);
-                // console.log(displayName.innerHTML);
-            })
+            // display data
+            displayName.innerHTML = name;
+            displayIcon.setAttribute('src', `https://openweathermap.org/img/wn/${currentIcon}@2x.png`);
+            displayTemperature.innerHTML = `Current Temperature: ${currentTemp}`;
+            displayFeelsLike.innerHTML = `Feels like: ${currentFeelsLike}`;
+            displayTempMax.innerHTML = `High: ${currentTempMax}`;
+            displayTempMin.innerHTML = `Low: ${currentTempMin}`;
+            displayDescription.innerHTML = currentDescription;
+            displaySunrise.innerHTML = `Sunrise: ${currentSunrise}`;
+            displaySunset.innerHTML = `Sunset: ${currentSunset}`;
+            displayWindSpeed.innerHTML = `Wind speed: ${currentWindSpeed}`;
+            displayWindDirection.innerHTML = `Wind direction: ${currentWindDirection}`;
+            displayHumidity.innerHTML = `Humidity: ${currentHumidity}`;
+            displayPressure.innerHTML = `Pressure: ${currentPressure}`;
+            
+            // Add new city to recent history
+            const searchedCitiesList = Array.from(recentHistory.querySelectorAll("li.searched-city"));
+            const searchedCitiesIds = searchedCitiesList.map( city => city.getAttribute("id"));
+            
+            if (!searchedCitiesIds.includes(name)) {
+                let searchedCity = document.createElement("li");
+                searchedCity.setAttribute("class", "searched-city");
+                searchedCity.setAttribute("id", name);
+                searchedCity.innerHTML = name;
+                recentHistory.appendChild(searchedCity);
+            };
+        })
+}
 
-        fetch(fivedayforecast_api)
-            .then(response => response.json())
-            .then(data => {
-                console.log(data);
-            });
+// Search Bar including submit button
+const searchBar = document.getElementById("search-button");
+searchBar.addEventListener( 'click', (event) => {
+    event.preventDefault();
+    const searchBarInput = document.getElementById('city-search').value;
+    const countryCode = countrySelect.options[countrySelect.selectedIndex].value;
+    const state = stateSelect.options[stateSelect.selectedIndex].value;
+    const geocodingAPI = getGeocodingAPI(searchBarInput, state, countryCode)
+            getCoordinates(geocodingAPI);
+});
 
-    }
-
-})
