149c980e4b12063b0d44d3083d6ace64ab78f89c
Justin 석원 Choi <122292721+justinschoi93@users.noreply.github.com>
2024-03-25 22:00:16 -0700

    Update getGeocodingAPI.js

--- assets/src/scripts/getGeocodingAPI.js
+++ assets/src/scripts/getGeocodingAPI.js
@@ -5,13 +5,13 @@ export default async function getGeocodingAPI (searchBarInput, state, countryCod
 
     if (isValidZip(searchBarInput)) { // if input is a zip code
         let zipcode = searchBarInput;
-        geocoding_api = `http://api.openweathermap.org/geo/1.0/zip?zip=${zipcode}&appid=0b2949d3ba17a6aec298126cb969f7dc`
+        geocoding_api = `https://api.openweathermap.org/geo/1.0/zip?zip=${zipcode}&appid=0b2949d3ba17a6aec298126cb969f7dc`
     } else if (!state === 'Select a state (optional)') {
         let city = searchBarInput;
-        geocoding_api = `http://api.openweathermap.org/geo/1.0/direct?q=${city},${state},${countryCode}&appid=0b2949d3ba17a6aec298126cb969f7dc`
+        geocoding_api = `https://api.openweathermap.org/geo/1.0/direct?q=${city},${state},${countryCode}&appid=0b2949d3ba17a6aec298126cb969f7dc`
     } else {
         let city = searchBarInput;
-        geocoding_api = `http://api.openweathermap.org/geo/1.0/direct?q=${city},${countryCode}&appid=0b2949d3ba17a6aec298126cb969f7dc`
+        geocoding_api = `https://api.openweathermap.org/geo/1.0/direct?q=${city},${countryCode}&appid=0b2949d3ba17a6aec298126cb969f7dc`
     }
     return geocoding_api;
     // return   `http://api.openweathermap.org/geo/1.0/direct?q=Seoul,410&appid=0b2949d3ba17a6aec298126cb969f7dc`;
